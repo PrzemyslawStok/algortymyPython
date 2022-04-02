b6e10da20b444bf4374414959bd1a9ee0fb29ddd
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-04-02 12:16:00 +0200

    cd..

--- macierze.py
+++ macierze.py
@@ -1,5 +1,11 @@
 import numpy as np
 import timeit
 
+
+def macierze1():
+    A = np.array([[1, 2, 0], [0, 1, 1], [1, 0, 1]])
+    print(A)
+
+
 if __name__ == "__main__":
-    pass
+    macierze1()
