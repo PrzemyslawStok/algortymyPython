50396039c656d79606f010d5b3b38151d1f0909e
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-04-09 12:11:36 +0200

    cd..

--- macierze.py
+++ macierze.py
@@ -12,10 +12,52 @@ def macierze1():
     print(np.eye(5))
 
     for i in range(len(A)):
-        A[i] = 5
+        A[i, i] = 5
 
     print(A)
 
 
+def macierze2():
+    A = np.eye(5)
+    print(A)
+
+    print(np.ones([10, 2]))
+    print(np.zeros([5, 3]))
+
+    print(5 * np.ones([10, 2]))
+
+    B = np.zeros([5, 3])
+    print(B)
+
+    print(B.fill(2))
+    print(B)
+
+    print(np.diag([1, 3, 4, 7, 1]))
+
+
+def macierze3():
+    A = np.arange(1, 10)
+    print(A)
+
+    A = np.arange(1, 10, 2)
+    print(A)
+
+    A = np.arange(0, 100)
+    B = A.reshape([10, -1])
+
+    C = B * B
+
+    print(C)
+
+    for i in range(len(C)):
+        C[i, i] = 0
+
+    print(C)
+
+    print(np.flip(C, axis=0))
+
+
 if __name__ == "__main__":
-    macierze1()
+    # macierze1()
+    # macierze2()
+    macierze3()
