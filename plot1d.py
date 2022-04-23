8bb12f7dbf59be4d1094382789ce092731c1ff52
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-04-23 15:15:05 +0200

    cd..

--- plot1d.py
+++ plot1d.py
@@ -1,13 +1,11 @@
 from matplotlib import pyplot as plot
 import numpy as np
 
-if __name__ == "__main__":
+
+def wykres1():
     X = np.linspace(0, 5, 100)
     Y = X
 
-    # int(x)
-    # round(x)
-    # ceil(x)
     Y1 = [int(x) for x in X]
     Y2 = np.round(X)
     Y3 = np.ceil(X)
@@ -19,3 +17,17 @@ if __name__ == "__main__":
 
     plot.legend()
     plot.show()
+
+
+def wykres2():
+    t = np.linspace(0, 2 * np.pi, 100)
+    X = np.sin(4 * t)
+    Y = np.cos(4 * t)
+
+    plot.plot(X, Y)
+    plot.show()
+
+
+if __name__ == "__main__":
+    # wykres1()
+    wykres2()
