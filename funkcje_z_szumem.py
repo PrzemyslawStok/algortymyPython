e2750c3fb83b8686ad433dd1574cac9c20ef5178
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-06-04 13:31:48 +0200

    cd..

--- funkcje_z_szumem.py
+++ funkcje_z_szumem.py
@@ -3,8 +3,8 @@ import numpy as np
 import tensorflow as tf
 
 
-def data():
-    X = np.linspace(0, 2 * np.pi, 100)
+def data(n: int = 100):
+    X = np.linspace(0, 2 * np.pi, n)
     Y = np.sin(X)
 
     noise_lvl = 0.1
@@ -14,7 +14,7 @@ def data():
 
 
 if __name__ == "__main__":
-    X, Y_f, Y_noise = data()
+    X, Y_f, Y_noise = data(2)
 
     plot.plot(0, 0)
     plot.plot(X, Y_f, label="$y=sin(x)$")
@@ -22,3 +22,13 @@ if __name__ == "__main__":
 
     plot.legend()
     plot.show()
+
+    model = tf.keras.Sequential()
+
+    model.add(tf.keras.layers.InputLayer(input_shape=np.shape(X)))
+    model.add(tf.keras.layers.Dense(5))
+    model.add(tf.keras.layers.Dense(1))
+
+    model.summary()
+
+
