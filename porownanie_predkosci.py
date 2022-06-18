6c5aeb9995d77bc2ee9d9e394d7a0e6f634bef3e
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-06-18 12:33:55 +0200

    cd..

--- porownanie_predkosci.py
+++ porownanie_predkosci.py
@@ -1,7 +1,19 @@
 import timeit
 
-if __name__ == "__main__":
+
+def f0():
+    for i in range(1000_000):
+        a = i * i
+    return 0.0
+
+
+def zmierz_czas(f):
     start = timeit.default_timer()
-    print(f"start: {start}")
+    f()
     end = timeit.default_timer()
-    print(f"start: {end}")
+    t = end - start
+    print(f"czas działania: {t:0.5f}")
+
+
+if __name__ == "__main__":
+    zmierz_czas(f0)
