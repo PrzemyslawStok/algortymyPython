d06752e55b5a6c49b020740c2e59090711f12aa6
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-06-25 12:22:08 +0200

    cd..

--- funkcje_cwiczenia.py
+++ funkcje_cwiczenia.py
@@ -4,10 +4,34 @@ from random import random
 import numpy as np
 from numba import jit
 
+def tuple_list():
+    A = [1, 2, 3, 4, 5]
+    print(A)
+
+    B = (1, 2, 3, 4, 5)
+    print(B)
+
+    for b in B:
+        print(b)
+
+    B1 = list(B)
+    print(B1)
+
+    B1[2] = 100
+
+    print(B1)
+
+    B2 = tuple(B1)
+    print(B2)
+
 
 def f0():
-    return 0
+    return 10, 1
 
 
 if __name__ == "__main__":
-    f0()
+    print(f0())
+
+    tuple_list()
+
+
